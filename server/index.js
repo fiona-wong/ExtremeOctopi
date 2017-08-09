051c6237f95368dca5ca940a28d39b3c6a6376f2
laraismael1 <laraismael123@gmail.com>
2017-08-08 19:48:33 -0700

    added database functionality

--- server/index.js
+++ server/index.js
@@ -1,80 +1,50 @@
-var data = require( '../database-mongo' );
-var bodyParser = require( 'body-parser' );
+var db = require( '../database-mongo' );
+var body = require( 'body-parser' );
 var express = require( 'express' );
 
 var app = express();
 
 app.use( express.static( __dirname + '/../react-client/dist' ) );
 
-// post request -login
-  // connect to DB to check passward againt the user name
-request.post('/login', (err, res, data) => {
-	if (err) {console.error(err)}	
-	
-	//db.func
-	res.status(201).send(JSON.parse(data))	
+
+// wating for authentication
+request.post('/login', (req, res) => {
+		//db.func
+		res.status(201).send(JSON.parse(data));	
 })
 
-// post request -signup
-	// send to DB user name/ password / full name / email
-	request.post('/signup', (err, res, data) => {
-	if (err) {console.error(err)}	
-	
-	//db.func
-	res.status(201).send(JSON.parse(data))	
+	// wating for authentication
+	request.post('/signup', (req, res) => {
+		//db.func
+		res.status(201).send(JSON.parse(data));	
 })
 
-// post request -test
-	// send to DB test results
-request.post('/test', (err, res, data) => {
-	if (err) {console.error(err)}	
-	
-		//db.func
-	res.status(201).send(JSON.parse(data))	
+request.post('/test', (req, res) => {
+	db.testResults(req.body.username, req.body.results);
 })	
 
-// post request messages
-	// user id / friend id / message
-	request.post('/message', (err, res, data) => {
-	if (err) {console.error(err)}	
-	
-		//db.func
-	res.status(201).send(JSON.parse(data))	
+request.post('/message', (req, res) => {
+	db.postMessage(req.body.sender, req.body.receiver, req.body.message);	
 })	
 
-// get request matching page
-	// user table
-	// matches
-request('/matching', (req, res) => {
-	if (err) {console.error(err)}	
-	
-	//db.func
-	res.status(200).send(JSON.parse(req.body))	
+request.get('/matching', (req, res) => {
+	db.getMatches(req.body.username, (matches) => {
+		res.status(200).send(JSON.parse(matches));	
+	})
 })	
 
-// get request profile page
-	// user table
-request('/profile', (req, res) => {
-	if (err) {console.error(err)}	
-	
-	//db.func
-	res.status(200).send(JSON.parse(req.body))	
+request.get('/profile', (req, res) => {
+	db.getProfile(req.body.username, (profile) => {
+		res.status(200).send(JSON.parse(profile));	
+	})
 })		
 	
-
-// get request messages
-	// user id / friend id / message
-request('/message', (req, res) => {
-	if (err) {console.error(err)}	
-	
-	//db.func
-	res.status(200).send(JSON.parse(req.body))	
+request.get('/message', (req, res) => {
+	db.getMessages(req.body.user, (messageObj) =>{
+		res.status(200).send(JSON.parse(messageObj));	
+	})
 })	
 
-
-app.listen( 3000, function() {
-  console.log( 'listening on port 3000!' );
-} );
 
 app.listen( 3000, function() {
   console.log( 'listening on port 3000!' );

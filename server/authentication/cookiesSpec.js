b074282f36b92ecc0b5a42b1468515c01d295618
Hack Reactor Student <student@hackreactor.com>
2017-08-10 18:48:00 -0700

    Test cookies.js
    Write tests for cookies.js

--- server/authentication/cookiesSpec.js
+++ server/authentication/cookiesSpec.js
@@ -1,2 +1,44 @@
 var expect = require( 'chai' ).expect;
+var httpMocks = require( 'node-mocks-http' );
 var cookies = require( './cookies.js' );
+
+describe( 'Sessions', () => {
+  it( 'should bake random cookies', () => {
+    var aCookie = cookies.bakeCookies();
+    var bCookie = cookies.bakeCookies();
+
+    expect( aCookie ).to.be.an( 'string' );
+    expect( aCookie ).to.not.equal( bCookie );
+  } );
+
+  it( 'should parse cookies', () => {
+    var requestWithoutCookies = httpMocks.createRequest();
+    var requestWithCookies = httpMocks.createRequest(
+      { headers: { Cookie: 'hotate=cCookie; takoyaki=aCookie; tamago=bCookie' } }
+    );
+
+    cookies.parseCookies( requestWithoutCookies, () => {
+      var cookies = requestWithoutCookies.cookies;
+      var expectedCookies = {};
+
+      expect( cookies ).to.be.an( 'object' );
+      expect( cookies ).to.eql( expectedCookies );
+    } );
+
+    cookies.parseCookies( requestWithCookies, () => {
+      var cookies = requestWithCookies.cookies;
+      var expectedCookies = { hotate: 'cCookie', takoyaki: 'aCookie', tamago: 'bCookie' };
+
+      expect( cookies ).to.be.an( 'object' );
+      expect( cookies ).to.eql( expectedCookies );
+    } );
+  } );
+
+  xit( 'should create sessions', () => {
+    console.log( 'IN PROGRESS' );
+  } );
+
+  xit( 'should verify sessions', () => {
+    console.log( 'IN PROGRESS' );
+  } );
+} );

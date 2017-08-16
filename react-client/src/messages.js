d232a3ebff737173d47eab5f3f2ede0940680c47
Hack Reactor Student <student@hackreactor.com>
2017-08-16 10:22:04 -0700

    Feat/messages
    Implement a non-tested version of feat/messages

--- react-client/src/messages.js
+++ react-client/src/messages.js
@@ -1,9 +1,74 @@
-import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+import React from 'react';
+import $ from 'jquery';
 
-class Message extends Component {
-    render(){
-        return (<h1>Message page</h1>);
+class Messages extends React.Component {
+  constructor( props ) {
+    super( props );
+
+    this.state = {
+      user: '',
+      match: '',
+      message: '',
+      messages: []
     }
+
+    this.updateMessages();
+  }
+
+  updateMessages() {
+    $.ajax( {
+      method: 'GET',
+      url: '/message',
+      data: { user: this.state.user },
+      success: ( data ) => {
+        console.log( 'SUCCESS:', data );
+
+        this.setState( { messages: data } );
+      },
+      error: ( error ) => {
+        console.log( 'ERROR:', error );
+      }
+    } );
+  }
+
+  onChange( event ) {
+    this.setState( { message: event.target.value } );
+  }
+
+  onClick() {
+    this.state.messages.push( this.state.message );
+
+    $.ajax( {
+      method: 'POST',
+      url: '/message',
+      data: { sender: this.state.user,
+              receiver: this.state.match,
+              message: this.state.message
+            },
+      success: ( data ) => {
+        console.log( 'SUCCESS:', data );
+
+        this.updateMessages();
+      },
+      error: ( error ) => {
+        console.log( 'ERROR:', error );
+      }
+    } );
+  }
+
+  render() {
+    return (
+      <div>
+        { this.state.messages.map( ( message, index ) => {
+            return <div key={ index }>{ message }</div>;
+          } ) }
+        <br></br>
+        <input onChange={ this.onChange.bind( this ) }></input>
+        <button onClick={ this.onClick.bind( this ) }>Submit</button>
+      </div>
+    );
+  }
 }
 
-export default Message
+export default Messages

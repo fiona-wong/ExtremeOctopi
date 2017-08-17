10ec328a1a3860ffa46ed0e27608389e5947fbaf
Hack Reactor Student <student@hackreactor.com>
2017-08-16 20:14:53 -0700

    Bug found in getUserCookie
    Made some major fixes but getUserCookie should work now

--- database-mongo/index.js
+++ database-mongo/index.js
@@ -375,14 +375,12 @@ var getMessages = function (user, callback) {
   });
 };
 
-
-var getCookieUser = function (cookie, callback) {
-  User.findOne({cookies: cookie}, (err, matches) => {
-    if (matches) {
-      callback({username: matches.username, cookies: cookie});
-      return {username: matches.username};
+var getCookieUser = function (req, cookie) {
+  User.find({cookies: cookie}, (error, matches) => {
+    if (matches.length !== 0) {
+      req.session = {username: matches[ 0 ].username, cookies: cookie};
     } else {
-      return {};
+      req.session = {};
     }
   });
 };

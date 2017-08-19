684b3a157c6d0be851ab93faa4eef38986d5f42f
Andy Lien <andyhlien@gmail.com>
2017-08-19 03:58:39 -0700

    Refactor Profile.jsx, implement live data
    Clean up code and HTML
    Feed /Profile endpoint live data

--- react-client/src/Profile.jsx
+++ react-client/src/Profile.jsx
@@ -1,64 +1,66 @@
 import React from 'react';
 
 class Profile extends React.Component {
+	constructor( props ) {
+  	super( props );
 
-	constructor(props) {
-    	super(props);
+  	var regexp = /^\/Profile\/(.*)\/$/;
+  	var user = props.history.location.pathname.match( regexp )[ 1 ];
 
-    	var regexp = /^\/Profile\/(.*)\/$/;
-    	var user = props.history.location.pathname.match(regexp)[1];
+  	this.state = {
+  		profilePic: 'https://s-media-cache-ak0.pinimg.com/originals/36/43/e7/3643e7e8dab9b88b3972ee1c9f909dea.jpg',
+      username: '(ノ°Д°）ノ︵ ┻━┻ | Please log in',
+  		fullname: 'Not Logged In',
+  		location: '(ノ°Д°）ノ︵ ┻━┻ | Please log in',
+  		hobbies: '(ノ°Д°）ノ︵ ┻━┻ | Please log in',
+  		aboutme: '(ノ°Д°）ノ︵ ┻━┻ | Please log in',
+  	}
 
-    	console.log(user);
-
-    	this.state = {
-    		profilePic: '',
-    		name: '',
-    		location: '',
-    		hobbies: '',
-    		aboutme: ''
-    	}
+    this.getProfile();
 	}
 
- componentDidMount () {
-    $.ajax({
-      url: '/profile',
+  getProfile() {
+    $.ajax( {
       method: 'GET',
+      url: '/profile',
       success: ( data ) => {
-        var data = JSON.parse(data);
-        console.log(data);
-        this.setState ({
-        	profilePic: 'https://s-media-cache-ak0.pinimg.com/originals/36/43/e7/3643e7e8dab9b88b3972ee1c9f909dea.jpg',
-        	name: 'Fiona',
-        	location: 'San Francisco',
-        	hobbies: 'doing things',
-        	aboutme: 'I like doing things'
-        });
+        console.log( 'SUCCESS:', data );
+
+        var data = JSON.parse( data );
+
+        this.setState ( {
+          profilePic: data.img || 'https://s-media-cache-ak0.pinimg.com/originals/36/43/e7/3643e7e8dab9b88b3972ee1c9f909dea.jpg',
+          username: data.username || '~(>_<~)',
+          fullname: data.fullname || '~(>_<~)',
+          location: data.location || '~(>_<~)',
+          hobbies: data.hobbies || '~(>_<~)',
+          aboutme: data.blog || '~(>_<~)'
+        } );
       },
       error: ( error ) => {
-        //debug
-        console.log( 'ERROR: ', error );
+        console.log( 'ERROR:', error );
       }
-    });
+    } );
   }
 
   render () {
     return (
-    <div className="container">
-
-			  <img className="img-circle" src={this.state.profilePic}></img>
-
-        <div className="col-xs-6 center-text">
-    	    <h1>{this.state.name}'s Zone</h1>
-    	    <br />
-    			<strong>Location: &nbsp;</strong>{this.state.location}
-					<br />
-          <strong>Hobbies/Interests:&nbsp; </strong>{this.state.hobbies}
-          <br />
-          <strong>About Me:&nbsp; </strong>{this.state.aboutme}
-          <br />
-          <button className="message-button"><img src="logo.png" width="30" height="30" className="d-inline-block align-top" alt=""/>Send {this.state.name} a message!</button>
-        </div>
-    </div>
+      <div className="container">
+  			  <img className="img-circle" src={ this.state.profilePic }></img>
+          <div className="col-xs-6 center-text">
+      	    <h1>{ this.state.fullname }'s Zone</h1>
+            <br/>
+            <strong>Username:&nbsp;</strong>{ this.state.username }
+      	    <br/>
+      			<strong>Location:&nbsp;</strong>{ this.state.location }
+  					<br/>
+            <strong>Hobbies:&nbsp;</strong>{ this.state.hobbies }
+            <br/>
+            <strong>About Me:&nbsp;</strong>{ this.state.aboutme }
+            <br/>
+            <button className="message-button"><img src="logo.png" width="30" height="30" className="d-inline-block align-top" alt=""/>Send { this.state.fullname } a message!</button>
+          </div>
+      </div>
     );
   }
 }

453248a571104b82ceac640a8c671cd905709e3f
Andy Lien <andyhlien@gmail.com>
2017-08-19 03:55:25 -0700

    Refactor Matches.jsx spacing, feat/Matches
    Clean up code and HTML
    Implement matching with dummy data
    Bug found in postUserMatches

--- react-client/src/Matches.jsx
+++ react-client/src/Matches.jsx
@@ -1,92 +1,122 @@
 import EditProfile from './components/EditProfile.jsx';
 import MatchesList from './components/MatchesList.jsx';
+import ImageUpload from './components/UploadPic.jsx';
 import React from 'react';
 import $ from 'jquery';
-import ImageUpload from './components/UploadPic.jsx';
-
 
 class Matches extends React.Component {
-  constructor(props) {
-    super(props);
-
-    var matchList = [
-      {name: 'Alan Turning', pic: 'https://s-media-cache-ak0.pinimg.com/originals/36/43/e7/3643e7e8dab9b88b3972ee1c9f909dea.jpg'},
-      {name: 'Tom Cruz', pic: 'https://s-media-cache-ak0.pinimg.com/originals/36/43/e7/3643e7e8dab9b88b3972ee1c9f909dea.jpg'},
-      {name: 'Albert Einstein', pic: 'https://s-media-cache-ak0.pinimg.com/originals/36/43/e7/3643e7e8dab9b88b3972ee1c9f909dea.jpg'},
-      {name: 'Pam Yanez', pic: 'https://s-media-cache-ak0.pinimg.com/originals/36/43/e7/3643e7e8dab9b88b3972ee1c9f909dea.jpg'},
-      {name: 'Mike Colson', pic: 'https://s-media-cache-ak0.pinimg.com/originals/36/43/e7/3643e7e8dab9b88b3972ee1c9f909dea.jpg'},
-      {name: 'Winnie Pooh', pic: 'https://s-media-cache-ak0.pinimg.com/originals/36/43/e7/3643e7e8dab9b88b3972ee1c9f909dea.jpg'}
-
-    ];
+  constructor( props ) {
+    super( props );
 
     this.state = {
       name: '',
       hobbies: '',
       aboutme: '',
-      matches: matchList
+      matches: [
+        { name: 'Not Logged In', pic: 'https://s-media-cache-ak0.pinimg.com/originals/36/43/e7/3643e7e8dab9b88b3972ee1c9f909dea.jpg' },
+        { name: 'Not Logged In', pic: 'https://s-media-cache-ak0.pinimg.com/originals/36/43/e7/3643e7e8dab9b88b3972ee1c9f909dea.jpg' },
+        { name: 'Not Logged In', pic: 'https://s-media-cache-ak0.pinimg.com/originals/36/43/e7/3643e7e8dab9b88b3972ee1c9f909dea.jpg' },
+        { name: 'Not Logged In', pic: 'https://s-media-cache-ak0.pinimg.com/originals/36/43/e7/3643e7e8dab9b88b3972ee1c9f909dea.jpg' },
+        { name: 'Not Logged In', pic: 'https://s-media-cache-ak0.pinimg.com/originals/36/43/e7/3643e7e8dab9b88b3972ee1c9f909dea.jpg' },
+        { name: 'Not Logged In', pic: 'https://s-media-cache-ak0.pinimg.com/originals/36/43/e7/3643e7e8dab9b88b3972ee1c9f909dea.jpg' }
+      ]
     };
 
-    this.handleChange = this.handleChange.bind(this);
-    this.handleEditProfile = this.handleEditProfile.bind(this);
+    this.postMatches();
   }
 
-  // componentDidMount () {
-  //   $.ajax({
-  //     url: '/matches',
-  //     method: 'GET',
-  //     success: (data) => {
-  //       data = JSON.parse(data);
-  //       this.setState({
-  //         name: data.name,
-  //         hobbies: data.hobbies,
-  //         aboutme: data.aboutme,
-  //         matches: data.matches
-  //       })
-  //     },
-  //     error: (error) => {
-  //       //debug
-  //       console.log( 'ERROR: ', error );
-  //     }
-  //   });
-  // }
+  postMatches() {
+    Matches = this;
 
+    $.ajax( {
+      method: 'POST',
+      url: '/matches',
+      data: {
+        numberToReturn: 5,
+        maxFriends: 5,
+      },
+      success: ( data ) => {
+        console.log( 'SUCCESS', data[ 0 ] );
 
-  handleEditProfile(event) {
-    event.preventDefault();
-
-    $.post('updateUser', this.state, (res) => {
-      if(res === 'Success') {
-
-      } else {
-        console.log('db not updated');
+        Matches.getMatches();
+      },
+      error: ( error ) => {
+        console.log( 'ERROR:', error );
       }
-    })
+    } );
   }
 
-  handleChange(event) {
+  getMatches() {
+    $.ajax( {
+      method: 'GET',
+      url: '/matches',
+      success: ( data ) => {
+        console.log( 'SUCCESS', data );
+
+        var data = JSON.parse( data );
+
+        if ( data ) {
+          if ( data.length === 0 ) {
+            data = [
+              { name: '｡ﾟ(*´□`)ﾟ｡', pic: 'https://s-media-cache-ak0.pinimg.com/originals/36/43/e7/3643e7e8dab9b88b3972ee1c9f909dea.jpg' },
+              { name: '｡ﾟ(*´□`)ﾟ｡', pic: 'https://s-media-cache-ak0.pinimg.com/originals/36/43/e7/3643e7e8dab9b88b3972ee1c9f909dea.jpg' },
+              { name: '｡ﾟ(*´□`)ﾟ｡', pic: 'https://s-media-cache-ak0.pinimg.com/originals/36/43/e7/3643e7e8dab9b88b3972ee1c9f909dea.jpg' },
+              { name: '｡ﾟ(*´□`)ﾟ｡', pic: 'https://s-media-cache-ak0.pinimg.com/originals/36/43/e7/3643e7e8dab9b88b3972ee1c9f909dea.jpg' },
+              { name: '｡ﾟ(*´□`)ﾟ｡', pic: 'https://s-media-cache-ak0.pinimg.com/originals/36/43/e7/3643e7e8dab9b88b3972ee1c9f909dea.jpg' },
+              { name: '｡ﾟ(*´□`)ﾟ｡', pic: 'https://s-media-cache-ak0.pinimg.com/originals/36/43/e7/3643e7e8dab9b88b3972ee1c9f909dea.jpg' }
+            ]
+          }
+
+          this.setState( {
+            matches: data
+          } );
+        }
+      },
+      error: ( error ) => {
+        console.log( 'ERROR:', error );
+      }
+    } );
+  }
+
+  handleEditProfile( event ) {
     event.preventDefault();
+
+    $.post( 'updateUser', this.state, ( data ) => {
+      var data  = JSON.parse( data );
+
+      if( data ) {
+        console.log( 'SUCCESS:', data );
+      } else {
+        console.log( 'ERROR:', data );
+      }
+    } );
+  }
+
+  handleChange( event ) {
+    event.preventDefault();
+
     const target = event.target;
     const name = target.name;
-    this.setState({
-      [name]: target.value
-    });
-  }
 
+    this.setState( {
+      [ name ]: target.value
+    } );
+  }
 
   render() {
     return (
       <div>
         <div className="row">
           <div className="col-md-4 col-md-offset-1">
-            <ImageUpload />
+            <ImageUpload/>
           </div>
           <div className="col-md-7">
-            <EditProfile handleChange={this.handleChange} handleEditProfile={this.handleEditProfile}/>
+            <EditProfile handleChange={ this.handleChange.bind( this ) } handleEditProfile={ this.handleEditProfile.bind( this ) }/>
           </div>
         </div>
         <div className="row">
           <div className="matches-list col-lg-offset-1">
-            <MatchesList matches={this.state.matches}/>
+            <MatchesList matches={ this.state.matches }/>
           </div>
         </div>
         <div className="row get-messages">
